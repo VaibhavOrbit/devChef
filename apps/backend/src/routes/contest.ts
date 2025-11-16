eeefc5913eae9a0997c747b7e0dccf1ace6f08bf
VaibhavOrbit <vaibhav.readme@gmail.com>
2025-11-16 17:50:35 +0000

    todo : complete all the endpoints

--- apps/backend/src/routes/contest.ts
+++ apps/backend/src/routes/contest.ts
@@ -7,11 +7,27 @@ router.get("/active", (req, res)=>{
 
 })
 
-
 router.get("/finished", (req,res)=>{
     const {offset, page} = req.query; 
 
 }) 
+router.get("/:contestId", (req,res)=> {
+    const contestId  = req.params.contestId; 
+     
+})
+
+router.get("/:contestId/:challengseid", (req,res)=> {
+    const contestId = req.params.contestId
+})
+
+router.get("/leaderboard/:contestId", (req,res)=>{
+
+})
+
+router.post("/submit/:challengeId", (req,res)=>{
+
+})
+
 
 
 export default router; 
--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -1,17 +1,41 @@
 import { Router } from "express";
 import {prismaClient} from "@repo/db/client"
+import { SignupSchema } from "../type";
+import { jwt } from "zod";
 const router: Router = Router(); 
 
-router.post("/signup", (req, res)=>{ 
-    const {username, password} = req.body(); 
+router.post("/signin", async(req, res)=>{ 
+    const {success, data} = SignupSchema.safeParse(req.body());
 
-})
+    if(!success){
+        res.status(411).json({
+            message:"Incorrect email format"
+        })
+
+        return;
+    }
+
+    const user = await prismaClient.user.upsert({
+        create : {
+            email: data.email,
+            role: "User"
+        },
+        update: {},
+        where: {
+            email: data.email
+        }
+
+    });
+    
+    
 
 
-router.post("/signin", (req, res)=>{ 
-    const {username, password} = req.body(); 
+
 
     
+
 })
+
+
 
 export default router; 
