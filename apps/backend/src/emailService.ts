0ea34acd700aba17b44b60e30612d535784fa6d4
VaibhavOrbit <vaibhav.readme@gmail.com>
2025-11-17 14:58:30 +0000

    created signin endpoint

---
+++ apps/backend/src/emailService.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+export function emailService(to:string, subject:string, textBody:string){
+    
+
+
+
+
+
+
+
+
+
+
+}
--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {prismaClient} from "@repo/db/client"
 import { SignupSchema } from "../type";
-import { jwt } from "zod";
+import  jwt, { JwtPayload }  from "jsonwebtoken";
+import { emailService } from "../emailService";
+
 const router: Router = Router(); 
 
 router.post("/signin", async(req, res)=>{ 
@@ -26,16 +28,62 @@ router.post("/signin", async(req, res)=>{
         }
 
     });
-    
-    
+        
+    const token = jwt.sign({
+        userId: user.id
+    }, process.env.EMAIL_JWT_PASSWORD!); 
 
-
-
-
+    if(process.env.NODE_ENV==="production"){
+        await emailService(data.email, `Login to Contest platform`, `Click this link to login ${process.env.FRONTEND_URL!}/user/login/post/?token=${token}`)
+    } else {
+        console.log(`The link for ${data.email} to login is http://localhost:3000/user/login/post?token=${token}`)
+    }
+     
+    res.json({
+        message: "We have emailed the one time login link to you, please check your email"
+    })
     
 
 })
 
+
+router.get("/signin/post", async (req , res) => {
+    try{
+        const token = req.query.token as string; 
+        const decoded = jwt.verify(token, process.env.EMAIL_JWT_PASS! ) as JwtPayload;
+        if(decoded.userId){
+            const user = await prismaClient.user.findFirst({
+                where: {
+                    id: decoded.userId
+                }
+            }); 
+
+            if(!user) { 
+                res.status(411).json({
+                    message: "Incorrect token"
+                })
+                return; 
+            }
+
+            const token = jwt.sign({
+                userId : decoded.userId, 
+                role: user.role
+            }, process.env.USER_JWT_PASS!)
+
+            res.json({
+                token, 
+            })
+        } else {
+            res.status(411).json({
+                message:"Incorrect token"
+            })
+        }
+        } catch(e){
+            res.status(411).json({
+                message: "Incorrect token"
+            })
+    }
+})
 
 
 export default router; 
